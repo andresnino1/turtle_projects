700ecac1dcec46060dffa719cc2102f24f56526d
andresnino1 <andresnino@gmail.com>
2020-03-09 14:19:06 +1100

    first change

---
+++ tortuga1.py
@@ -0,0 +1,20 @@
+# Step 1: Make all the "turtle" commands available to us.
+import turtle
+
+# Step 2: Create a new turtle. We'll call it "bob"
+bob = turtle.Turtle()
+
+# Step 3: Move in the direction Bob's facing for 50 pixels
+bob.forward(50)
+bob.forward(50)
+
+print("esto se debe imprimirbe imprimir")
+# Step 4: We're done!
+bob.forward(50)
+bob.forward(50)
+bob.forward(50)
+bob.forward(50)
+
+turtle.done()
+#nueva linea comentari
+# otra linea
